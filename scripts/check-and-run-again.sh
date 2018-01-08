7e455dc848e1b4c144d1120c7731eaaac051e848
Silvio Peroni <essepuntato@gmail.com>
2018-01-08 16:07:22 +0100

    Updated script

--- scripts/check-and-run-again.sh
+++ scripts/check-and-run-again.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 CUR_DIR="${pwd}"
 myv=`curl -s http://www.sparontologies.net`
-if [ -z "$myv" ]; then # If the ping does not resolve then run the server again
+if [[ -z "$myv" ]] || [[ $myv = "Traceback"*  ]]; then # If the ping does not resolve then run the server again
     ps -ef | grep "python spar.py" | awk '{print $2}' | xargs kill
     cd /home/essepuntato/websites
-    python spar.py 80 &
+    nohup python spar.py 80 &
+    date >> log.txt
     cd $CUR_DIR
 fi
