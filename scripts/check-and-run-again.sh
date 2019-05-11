21906fa53e9c406d25be9fe0ecf1e4df0a9005aa
Silvio Peroni <essepuntato@gmail.com>
2019-05-11 15:28:23 +0200

    Update check-and-run

--- scripts/check-and-run-again.sh
+++ scripts/check-and-run-again.sh
@@ -2,7 +2,9 @@
 myv=`curl -s http://www.sparontologies.net`
 
 if [[ -z "$myv" ]] || [[ $myv = "Traceback"*  ]]; then
-    /etc/init.d/lighttpd restart
+    /etc/init.d/lighttpd stop
+    pkill -f "python3 /var/www/"
+    /etc/init.d/lighttpd start
     exit 0
 fi
 
